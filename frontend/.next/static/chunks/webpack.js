a7b467a1ec79e44e60e5889c40ac119a3cbb0152
Emmanuel Okereke <eokereke47@gmail.com>
2022-05-23 15:29:35 +0100

    Navigation using link component

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ddd5416e0959cd81"; }
+/******/ 		__webpack_require__.h = function() { return "25a37072c274cbfa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
